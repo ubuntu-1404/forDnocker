1461eb57eb6ea0da696176af3249957def789c51
ubuntu-1404 <simul2013@126.com>
2015-07-08 09:36:45 +0800

    Update setup_mogo.sh
    add mongo replica set for configer

--- setup_mogo.sh
+++ setup_mogo.sh
@@ -23,3 +23,4 @@ fi
 if [ "${hname}" = "mongodbsharer.wodezoon.com" ]; then
         ${mongpath}/bin/mongod -fork -dbpath ${datapath} -logpath ${logpath}/data.log -replSet ${name}
 fi
+#rs.reconfig({_id : "testers1",members : [{_id : 0, host : "172.17.0.29:27017"},{_id : 1, host : "172.17.0.30:27017"},{_id : 2, host : "172.17.0.31:27017"}]})
