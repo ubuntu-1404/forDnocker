3ac560c88e33c933470792e6eb6280b97e68480e
ubuntu-1404 <simul2013@126.com>
2015-07-08 09:55:43 +0800

    Update setup_mogo.sh
    for arbiter set

--- setup_mogo.sh
+++ setup_mogo.sh
@@ -23,4 +23,6 @@ fi
 if [ "${hname}" = "mongodbsharer.wodezoon.com" ]; then
         ${mongpath}/bin/mongod -fork -dbpath ${datapath} -logpath ${logpath}/data.log -replSet ${name}
 fi
-#rs.reconfig({_id : "testers1",members : [{_id : 0, host : "172.17.0.29:27017"},{_id : 1, host : "172.17.0.30:27017"},{_id : 2, host : "172.17.0.31:27017"}]})
+#init.js==>
+#rs.reconfig({_id : "testers1",members:[{_id : 0, host : "172.17.0.29:27017",priority:2},{_id : 1, host : "172.17.0.30:27017",priority:1},{_id : 2, host : "172.17.0.31:27017", arbiterOnly:true}]})
+#init.js==>
