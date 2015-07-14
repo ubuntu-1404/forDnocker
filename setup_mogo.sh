89d0331a385e84b06c42d9f891689cf50dfdc05f
ubuntu-1404 <simul2013@126.com>
2015-07-14 12:38:49 +0800

    Update setup_mogo.sh
    add mongo-plugins

--- setup_mogo.sh
+++ setup_mogo.sh
@@ -94,3 +94,19 @@ fi
 #sed -i "/^mmsBaseUrl=/cmmsBaseUrl=http://${dbhost1}:20180"    /etc/mongodb-mms/monitoring-agent.config
 #/opt/mongodb-mms-automation/bin/mongodb-mms-automation-agent -f /etc/mongodb-mms/automation-agent.config
 #/opt/mongodb-mms-automation/bin/mongodb-mms-monitoring-agent -f /etc/mongodb-mms/monitoring-agent.config
+if [[ $(hostname) = mongodb.wodezoon.com* ]]; then
+        RockMongo_URL='https://github.com/iwind/rockmongo/archive/1.1.7.tar.gz'
+        echo 'start install apache2...'
+        apt-get install apache2 -y
+        echo "ServerName localhost:80" >> /etc/apache2/apache2.conf
+        /etc/init.d/apache2 restart
+        echo 'start install php5...'
+        apt-get install php5 libapache2-mod-php5 -y
+        apt-get install php5-dev -y
+        pecl install mongo
+        echo 'extension=mongo.so' >> /etc/php5/apache2/php.ini
+        /etc/init.d/apache2 restart
+        wget ${RockMongo_URL} --output-document=/tmp/rockmongo.tar.gz
+        tar -zxvf /tmp/rockmongo.tar.gz -C /var/www/html
+        echo 'success'
+fi
